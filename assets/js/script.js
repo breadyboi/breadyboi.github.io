db07ba8a767a65bd9b2e1564bb2f5d945c09dded
breadyboi <tano.brett@berkeley.edu>
2015-04-27 15:35:09 -0700

    everything

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#brettlogo').click(function() {
+    	$('.brettlogo_container').fadeOut(1000);
+  });
+});
+	$('.square').hover(function() {
+		$('this').fadeOut(1000);
+	}, function() {
+		$('this').fadeIn(1000);
+	});
